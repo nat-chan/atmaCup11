992523fabab30110f4a2b0f111b7cef20d75077a
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-07-21 10:40:12 +0900

    EfficentNetB0を試したがあんま良くない

--- models/networks/generator.py
+++ models/networks/generator.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from models.networks.base_network import BaseNetwork
 from torchvision.models import resnet34
+import timm
 from collections import defaultdict, OrderedDict
 
 class ResNet34Generator(BaseNetwork):
@@ -24,6 +25,32 @@ class ResNet34Generator(BaseNetwork):
                 for param in self.model.parameters(): # 最終層以外の重みを固定
                     param.requires_grad = False
         self.model.fc = nn.Linear(in_features=512, out_features=opt.out_features, bias=True)
+
+    def forward(self, input: torch.Tensor, z=None) -> torch.Tensor:
+        out = self.model(input)
+        return out
+
+class EfficientNetB0Generator(BaseNetwork):
+    def __init__(self, opt: argparse.Namespace) -> None:
+        super().__init__()
+        self.opt = opt
+        self.model = timm.create_model('efficientnet_b0', pretrained=False)
+        if not opt.transfer_weights == "":
+            weights = torch.load(opt.transfer_weights)
+            # 一時的に最終層のshapeをload元に合わせる
+            self.model.classifier = self.eff_fc_layer(weights["model.fc.weight"])
+            self.load_state_dict(weights)
+            if self.opt.transfer_freeze:
+                for param in self.model.parameters(): # 最終層以外の重みを固定
+                    param.requires_grad = False
+
+        self.model.classifier = self.eff_fc_layer(opt.out_features)
+
+    def eff_fc_layer(self, out_features: int):
+        fc_list = [nn.Linear(in_features=1280, out_features=32, bias=True),
+                   nn.Dropout(0.2),
+                   nn.Linear(in_features=32, out_features=out_features, bias=True)]
+        return nn.Sequential(*fc_list)
 
     def forward(self, input: torch.Tensor, z=None) -> torch.Tensor:
         out = self.model(input)
